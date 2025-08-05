5edb1bfc5b48c35e216f97b599dc844749bcd241
Projetxrs265465 <carlos.henriques720s@gmail.com>
2025-08-05 08:09:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,24 +25,6 @@ html {
 }
         `}</style>
       </head>
-      <!-- Event snippet for 12 conversion page -->
-<script>
-  gtag('event', 'conversion', {
-      'send_to': 'AW-17266114156/-dxECPHl0f8aEOz8j6lA',
-      'value': 1.0,
-      'currency': 'BRL',
-      'transaction_id': ''
-  });
-</script>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17266114156"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17266114156');
-</script>
       <body>{children}</body>
     </html>
   )
